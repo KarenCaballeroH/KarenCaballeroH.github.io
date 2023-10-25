8a583e2584e79836e31d90c24ba1b921233353b1
Karen Caballero <147679479+KarenCaballeroH@users.noreply.github.com>
2023-10-24 19:51:38 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+ cursor: "<span style='color: #76161b;'>|</span>",
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="color: #76161b;">Me gusta el turismo y aprender cosas nuevas.</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
